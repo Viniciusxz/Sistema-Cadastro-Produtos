f08ccc4d5198eab256da865a570b29f273365a0c
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-03-13 13:50:09 -0300

    Criação do menu do sistema de cadastro de produtos.

---
+++ Cadastro-Produtos.py
@@ -0,0 +1,11 @@
+#0 Criação do menu:
+print('Bem vindo ao sistema de cadastro de produtos! Oque você deseja fazer? (Digite o número da ação que deseja realizar.)')
+print('1. VER PRODUTOS | 2. CADASTRAR PRODUTO | 3. EXCLUIR PRODUTO  | 4. LIMPAR LISTAS DE PRODUTOS | 5. SAIR ')
+#1. Planejar o que é um produto (nome, codigo, quantidade, preço)
+#2. Criar armazenamento dos produtos (lista, listas de listas, dicionários)
+#3. Criação do menu do sistema: 1 - Ver produtos 2 - Cadastrar produto 3 - Excluir produto 4 - Limpar lista 5 - 
+#4. Função para cadastrar os produtos (pedir dados do produto, criar produto, adicionar na lista)
+#5. Função para ver os produtos (percorrer todos os produtos e mostrar na tela)
+#6. Função para excluir produtos (pedir nome do produto e depois remover da lista)
+#7. Função para apagar todos os produtos.
+#8. Conectar tudo no menu (ler a opção e chamar a função correta.)
