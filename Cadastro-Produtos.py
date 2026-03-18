9c9dbbe19272cefd5b5d50afb136ef66d77ff971
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-03-17 23:41:17 -0300

    Adaptando o sistema para um novo tipo de lista: Dicionário!

--- Cadastro-Produtos.py
+++ Cadastro-Produtos.py
@@ -2,36 +2,42 @@ import matplotlib.pyplot as plt
 
 #1 Criação das funções e listas antes do while, para o phyton reconhecer as funções que existem.
 lista_produtos = []
-
+contador_id = 1 
 
 def exibicao_produtos(): # Função que faz o usuário vizualizar todos os cadastros.
     if not lista_produtos:
         print('Nenhum produto foi cadastrado ainda!')
     else:
-        for numero, produto_ofc in enumerate(lista_produtos, start=1):
-                print(f"{numero} - {produto_ofc[0]} | Quantidade: {produto_ofc[1]} | Preço: R$ {produto_ofc[2]:.2f}")
+        for produto_ofc in (lista_produtos):
+                print(f"ID - {produto_ofc['id']} | Nome: {produto_ofc['nome']} | Quantidade: {produto_ofc['quantidade']} | Preço: R$ {produto_ofc['preco']:.2f}")
 
 def cadastro(): # Função que faz o usuário cadastrar seu produto.
+    global contador_id 
     produto_nome = verificar_nome_produto()
     if produto_nome == "0":
         return
-    produto_quantidade = verificar_quantidade_inteiro()
-    produto_preco = verificar_preco_float()
-    sublista = [produto_nome, produto_quantidade, produto_preco]
-    lista_produtos.append(sublista)
+    
+    dicionario_produto = {
+        "id": contador_id,
+        "nome": produto_nome,
+        "quantidade":verificar_quantidade_inteiro(),
+        "preco":verificar_preco_float()
+    } 
+    contador_id += 1
+    lista_produtos.append(dicionario_produto)
     print('Produto cadastrado com sucesso!')
 
 def atualizar_cadastro(): # Função que atualiza os status de um produto, quantidade e preço ( se quiser atualizar o preço )
-    atualizar_produto = str(input('Digite o nome do produto que deseja atualizar(0 para voltar ao menu): '))
+    atualizar_produto = int(input('Digite o ID do produto que deseja atualizar(0 para voltar ao menu): '))
     if atualizar_produto == "0":
         return
     atualizar_encontrado = False 
     for produto_atualizar in lista_produtos:
-        if atualizar_produto == produto_atualizar[0]:
+        if atualizar_produto == produto_atualizar["id"]:
             atualizar_quantidade = verificar_quantidade_inteiro()
             atualizar_preco = verificar_preco_float()
-            produto_atualizar[1] = atualizar_quantidade
-            produto_atualizar[2] = atualizar_preco
+            produto_atualizar["quantidade"] = atualizar_quantidade
+            produto_atualizar["preco"] = atualizar_preco
             print('Produto atualizado com sucesso!')
             atualizar_encontrado = True
             break
@@ -39,12 +45,12 @@ def atualizar_cadastro(): # Função que atualiza os status de um produto, quant
         print('Produto não encontrado!')
 
 def excluir_cadastro(): # Função que analisa se determinado produto está dentro da lista para remoção, se estiver, será apagado, se não, será alertado.
-    excluir_produto = str(input('Digite o nome do produto que deseja excluir(0 para voltar ao menu): '))
+    excluir_produto = int(input('Digite o ID do produto que deseja excluir(0 para voltar ao menu): '))
     if excluir_produto == "0":
         return
     produto_encontrado = False # Aqui utilizei a logica false, pois essa variavel é como um marcador para servir para a condição ter um pause, por isso há a existencia do break ali. Que será ativado quando o produto for encontrado. (true)
     for produto_excluir in lista_produtos:
-        if excluir_produto == produto_excluir[0]:
+        if excluir_produto == produto_excluir["id"]:
             lista_produtos.remove(produto_excluir)
             print('Produto removido com sucesso!')
             produto_encontrado = True
@@ -61,8 +67,8 @@ def exibir_grafico(): # Função que faz um gráfico (com mathplotlib) baseado n
     quantidade_produto = []
     
     for produto1 in lista_produtos:
-        nome_produto.append(produto1[0])
-        quantidade_produto.append(produto1[1])
+        nome_produto.append(produto1["nome"])
+        quantidade_produto.append(produto1["quantidade"])
     plt.bar(x = nome_produto, height = quantidade_produto)
     print('Exibindo o gráfico baseado no nome e quantidade do produto!')
     plt.show()
