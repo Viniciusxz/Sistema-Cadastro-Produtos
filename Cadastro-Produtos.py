416ac6a701012beea6e97d16bd69a5ffb615d434
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-03-17 21:54:12 -0300

    Adicionando condições nas verificações.

--- Cadastro-Produtos.py
+++ Cadastro-Produtos.py
@@ -84,7 +84,10 @@ def verificar_quantidade_inteiro():
     while True:
         try:
            verificar_inteiro = int(input('Digite a quantidade do produto aqui: '))
-           return verificar_inteiro
+           if verificar_inteiro >= 0:
+               return verificar_inteiro
+           else:
+               print('Quantidade inválida, não use números negativos!')
         except ValueError as erro0:
             print(f'Quantidade inválida, use apenas números! {erro0}')
 
@@ -92,7 +95,10 @@ def verificar_preco_float():
     while True:
         try:
             verificar_float = float(input('Digite o preço do produto aqui: '))
-            return verificar_float
+            if verificar_float >= 0:
+                return verificar_float
+            else:
+                print('Preço inválido! Não é permitido preço negativo.')
         except ValueError as erro1:
             print(f'Preço invalidado!, Digite em números e com . ao invés de , ! {erro1}')
 
